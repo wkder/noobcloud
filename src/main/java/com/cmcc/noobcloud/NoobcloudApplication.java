00049e173a553a7857c46702d6a36b610ea880af
dwk <dwkdwkdwkdwk@126.com>
2021-11-05 17:17:43 +0800

    1

---
+++ src/main/java/com/cmcc/noobcloud/NoobcloudApplication.java
@@ -0,0 +1,13 @@
+package com.cmcc.noobcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoobcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmcc/noobcloud/NoobcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmcc.noobcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
